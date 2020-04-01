cd75c87c74d53cde207a089800287b0f92ed8a12
vian <707820685@qq.com>
2020-04-01 20:58:36 +0800

    电子书相关功能

--- src/router/index.js
+++ src/router/index.js
@@ -67,10 +67,17 @@ export const asyncRoutes = [
         meta: { title: '上传图书', icon: 'edit', roles: ['admin'] }
       },
       {
+        name: 'bookEdit',
+        path: 'edit',
+        component: () => import('@/views/book/edit'),
+        hidden: true,
+        meta: { title: '编辑图书', icon: 'edit', roles: ['admin'], activeMenu: '/book/list' }
+      },
+      {
         name: 'bookList',
         path: 'list',
         component: () => import('@/views/book/create'),
-        meta: { title: '图书列表', icon: 'edit', roles: ['admin'] }
+        meta: { title: '图书列表', icon: 'list', roles: ['editor'] }
       }
     ]
   },
---
+++ src/views/book/components/Detail.vue
@@ -0,0 +1,59 @@
+<template>
+  <el-form ref="postForm" :model="postForm">
+    <stickey :class-name="'sub-navbar'">
+      <el-button v-if="!isEdit" @click="showGuide">显示帮助</el-button>
+      <el-button
+        v-loading="loading"
+        type="success"
+        style="margin-left: 10px"
+        @click="submitForm"
+      >
+        {{ isEdit ? '编辑电子书' : '新增电子书' }}
+      </el-button>
+    </stickey>
+    <div class="detail-container">
+      <el-row>
+        <warning />
+        <el-col :span="24">
+          <!-- 表单控件的具体样式 -->
+        </el-col>
+      </el-row>
+    </div>
+    <div />
+  </el-form>
+</template>
+
+<script>
+import Stickey from '@/components/Sticky'
+import Warning from './Warning'
+export default {
+  components: {
+    Stickey,
+    Warning
+  },
+  props: {
+    isEdit: Boolean
+  },
+  data() {
+    return {
+      loading: false,
+      postForm: {
+        status: ''
+      }
+    }
+  },
+  methods: {
+    submitForm() {
+      this.loading = true
+    },
+    showGuide() {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .detail-container {
+    padding: 40px 50px 20px;
+  }
+</style>
